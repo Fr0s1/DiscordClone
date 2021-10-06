e7d0ac66c9de1cfabab9574bcaeccd4f96d228ba
Hieu <dthieu223@gmail.com>
2021-10-06 09:51:16 +0700

    Add basic Socket.io path

--- ChatServer/src/server.js
+++ ChatServer/src/server.js
@@ -1,6 +1,6 @@
-const express = require('express'); 
-const app = express(); 
-const http = require('http'); 
+const express = require('express');
+const app = express();
+const http = require('http');
 const server = http.createServer(app);
 const cors = require('cors')
 
@@ -12,18 +12,22 @@ app.use(cors(corsOptions));
 
 require('./routes/message.route')(app)
 
-const io = require("socket.io")(server, {  
-    cors: {    
-        origin: "*",    
-        methods: ["GET", "POST"]  
-}});
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
-io.on("connection", (socket) => {
-    console.log('Connected')
-})
+const nsp = io.of('/chat')
+nsp.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        nsp.emit('chat message', msg);
+    });
+});
 
-app.get('/chat/healthz', (req, res) => { 
-    res.send(`🚀 Server ready at ${hostname}/chat`); 
+app.get('/chat/healthz', (req, res) => {
+    res.send(`🚀 Server ready at ${hostname}/chat`);
 });
 
 const PORT = process.env.PORT || 3000
