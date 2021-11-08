ad4944592468f7ce7bc779943e38172439a14036
Hieu <dthieu223@gmail.com>
2021-11-08 14:23:23 +0700

    Add changes to run in Docker Compose

--- FilesServer/src/models/index.js
+++ FilesServer/src/models/index.js
@@ -4,11 +4,20 @@ const userSchema = require('./User')
 
 const User = mongoose.model('User', userSchema)
 
-// let user = 'm001-student'
-// let password = 'm001-mongodb-basics'
-let database = 'discord'
-let host = 'localhost:27017'
-let connectionUri = `mongodb://${host}/${database}`
+let ENV = process.env.ENV
+let connectionUri
+let database = process.env.mongdb_database
+let mongodb_host = process.env.mongodb_host
+if (ENV == "AWS") {
+    let mongodb_user = process.env.mongodb_user
+    let mongodb_password = process.env.mongdb_password
+
+    connectionUri = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${database}?retryWrites=true&w=majority`
+} else {
+    let mongodb_port = process.env.mongodb_port
+    console.log(mongodb_port)
+    connectionUri = `mongodb://${mongodb_host}:${mongodb_port}/${database}`
+}
 
 main().catch(err => console.log(err));
 
