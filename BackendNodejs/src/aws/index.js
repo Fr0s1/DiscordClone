af699a305b3f0aba96b1fc093ed40b9199b0070c
Hieu <dthieu223@gmail.com>
2021-11-08 14:22:23 +0700

    Add changes to run in Docker Compose

--- BackendNodejs/src/aws/index.js
+++ BackendNodejs/src/aws/index.js
@@ -6,7 +6,28 @@ const cognitoConfig = {
 
 const bucketName = process.env.BUCKET_NAME
 
-const s3 = new AWS.S3({ params: { Bucket: bucketName } });
+const s3Config = {
+    params: { Bucket: bucketName },
+    region: 'ap-southeast-1'
+}
+
+let env = process.env.ENV
+
+if (env === "DOCKER") {
+    let acccessKeyId = process.env.accessKeyId
+    let secretAccessKey = process.env.secretAccessKey
+
+    s3Config.accessKeyId = acccessKeyId
+    s3Config.secretAccessKey = secretAccessKey
+
+    cognitoConfig.accessKeyId = acccessKeyId
+    cognitoConfig.secretAccessKey = secretAccessKey
+}
+
+console.log(s3Config)
+console.log(cognitoConfig)
+
+const s3 = new AWS.S3(s3Config);
 
 const cognitoClient = new AWS.CognitoIdentityServiceProvider(cognitoConfig)
 
--- BackendNodejs/src/graphql/resolvers/Subscription.js
+++ BackendNodejs/src/graphql/resolvers/Subscription.js
@@ -1,5 +1,19 @@
 const { RedisPubSub } = require('graphql-redis-subscriptions')
-const pubsub = new RedisPubSub();
+const Redis = require('ioredis');
+
+const options = {
+    host: process.env.redisHost,
+    port: process.env.redisPort,
+    retryStrategy: times => {
+        // reconnect after
+        return Math.min(times * 50, 2000);
+    }
+};
+
+const pubsub = new RedisPubSub({
+    publisher: new Redis(options),
+    subscriber: new Redis(options)
+});
 
 function accountStatusSubscribe() {
     return pubsub.asyncIterator(["ACCOUNT_STATUS_CHANGED"])
