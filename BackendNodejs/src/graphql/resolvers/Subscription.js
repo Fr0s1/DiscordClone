d433fb418698348176f9d54688c119484fbda960
Hieu <dthieu223@gmail.com>
2021-11-16 08:19:14 +0700

    Add subscription filter to publish account status changed event to right subscriber

--- BackendNodejs/src/graphql/resolvers/Subscription.js
+++ BackendNodejs/src/graphql/resolvers/Subscription.js
@@ -1,5 +1,6 @@
 const { RedisPubSub } = require('graphql-redis-subscriptions')
 const Redis = require('ioredis');
+const { withFilter } = require('graphql-subscriptions');
 
 const options = {
     host: process.env.redisHost,
@@ -19,8 +20,16 @@ function accountStatusSubscribe() {
     return pubsub.asyncIterator(["ACCOUNT_STATUS_CHANGED"])
 }
 
+const mongoUtilFunctions = require('../../mongodb/utils/utils')
+
 const accountStatusInfo = {
-    subscribe: accountStatusSubscribe,
+    subscribe: withFilter(accountStatusSubscribe,
+        async (payload, variables) => {
+            console.log(payload)
+            console.log(variables)
+            return await mongoUtilFunctions.ifInContactList(payload.username, variables.username)
+        }
+    ),
     resolve: payload => {
         return payload
     }
