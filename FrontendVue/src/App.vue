1d8de5b3c1206189b0ee289bcebfeb95ca247406
Payaso2767 <56871492+Payaso2767@users.noreply.github.com>
2021-11-21 09:17:00 +0700

    edit Sign in form

--- FrontendVue/src/App.vue
+++ FrontendVue/src/App.vue
@@ -1,23 +1,97 @@
 <template>
-  <div class="bgimg">
-    <amplify-authenticator>
-      <!-- The rest of your app code -->
+<div class="bgimg">
+  <amplify-authenticator>
+    <!-- The rest of your app code -->
 
-      <router-view />
-   
-    </amplify-authenticator>
-  </div>
+    <router-view />
+    <amplify-sign-in
+      header-text="Welcome to Discord Clone App"
+      slot="sign-in"
+      usernameAlias="email"
+      :formFields="formFields1"
+
+    ></amplify-sign-in>
+    <amplify-sign-up
+      slot="sign-up"
+      :formFields="formFields2"
+    >
+    <div slot="federated-buttons"></div></amplify-sign-up>
+  </amplify-authenticator>
+</div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'AuthWithSlots',
+  data() {
+    return {
+      
+      formFields1: [
+        {
+          type: 'username',
+          label: 'Username',
+          placeholder: 'Your Username',
+          inputProps: { required: true, autocomplete: 'username' },
+        },
+        {
+          type: 'password',
+          label: 'Password',
+          placeholder: 'Input Password',
+          inputProps: { required: true, autocomplete: 'new-password' },
+        },
+      ],
+      formFields2: [
+        {
+          type: 'name',
+          label: 'Your Name',
+          placeholder: 'Your Name',
+          inputProps: { required: true, autocomplete: 'name' },
+        },
+        {
+          type: 'birthdate',
+          label: 'Birthday',
+          placeholder: 'Your Birthday',
+          inputProps: { required: true, autocomplete: 'birthdate' },
+        },
+        {
+          type: 'email',
+          label: 'Email',
+          placeholder: 'Your Email',
+          inputProps: { required: true, autocomplete: 'email' },
+        },
+        {
+          type: 'username',
+          label: 'Username',
+          placeholder: 'Your Username',
+          inputProps: { required: true, autocomplete: 'username' },
+        },
+        {
+          type: 'password',
+          label: 'Password',
+          placeholder: 'Input Password',
+          inputProps: { required: true, autocomplete: 'new-password' },
+        },
+        {
+          type: 'phone_number',
+          label: 'Phone Number',
+          placeholder: 'Input your Phone Number',
+          inputProps: { required: true, autocomplete: 'phone_number' },
+        },
+      ]
+    }
+  }
+}
+
 </script>
+
 <style>
   :root{
-    --amplify-primary-color: #007bff;
-    --amplify-primary-tint: #2600ff;
-    --amplify-primary-shade: #1100ff;
+    --amplify-primary-color: #7289da;
+    --amplify-primary-tint: #99aab5;
+    --amplify-primary-shade: #007bff;
     --amplify-text-sm: 1rem;
+    --amplify-background-color: #2c2f33;
+     --amplify-secondary-color: #7289da;
   }
   .bgimg{
     background-image: url("./assets/LoginBackground.png");
